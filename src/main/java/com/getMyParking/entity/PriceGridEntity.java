7ec6bca77b102c80b6b6267e1fabf32db441a14a
Rahul Gupta <rahulgupta.s@flipkart.com>
2015-08-19 20:36:39 +0530

    adding json ignore

--- src/main/java/com/getMyParking/entity/PriceGridEntity.java
+++ src/main/java/com/getMyParking/entity/PriceGridEntity.java
@@ -1,5 +1,7 @@
 package com.getMyParking.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 /**
@@ -13,6 +15,7 @@ public class PriceGridEntity {
     private Integer cost;
     private Integer duration;
     private Integer sequenceNumber;
+    @JsonIgnore
     private PricingSlotEntity pricingSlot;
 
     @Id
