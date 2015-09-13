20f80fad5218204f6c600487fe0da04eaad3e3d9
Rahul Gupta <rahulgupta.s@flipkart.com>
2015-09-13 19:55:41 +0530

    foc, pass, sub lot type master table db changes

--- src/main/resources/db/migration/V1_1__SubLotTypeTableAddition.sql
+++ src/main/resources/db/migration/V1_1__SubLotTypeTableAddition.sql
@@ -13,4 +13,70 @@ ALTER TABLE `get_my_parking_v2`.`parking_event`
 ADD FOREIGN KEY (`sub_lot_type`)
 REFERENCES `get_my_parking_v2`.`sub_lot_type` (`type_name`);
 
+ALTER TABLE `get_my_parking_v2`.`parking_pass_master`
+ADD FOREIGN KEY (`vehicle_type`)
+REFERENCES `get_my_parking_v2`.`sub_lot_type` (`type_name`);
+
+ALTER TABLE `get_my_parking_v2`.`parking_pass_master`
+DROP COLUMN parking_sub_lot_id;
+
+ALTER TABLE `get_my_parking_v2`.`parking_pass_master`
+ADD COLUMN `parking_id` INT NOT NULL;
+
+ALTER TABLE `get_my_parking_v2`.`parking_pass_master`
+ADD FOREIGN KEY (`parking_id`)
+REFERENCES `get_my_parking_v2`.`parking` (`id`)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE `get_my_parking_v2`.`parking_pass`
+ADD COLUMN `operator_name` VARCHAR(255) NULL,
+ADD COLUMN `mobile_numbre` VARCHAR(255) NULL,
+ADD COLUMN `cost` INT NOT NULL,
+ADD COLUMN `is_paid` INT NOT NULL;
+
+ALTER TABLE `get_my_parking_v2`.`parking_event`
+ADD COLUMN `special` VARCHAR(255) NULL,
+ADD COLUMN `foc_reason` VARCHAR(255) NULL;
+
+ALTER TABLE `get_my_parking_v2`.`parking_event`
+ADD FOREIGN KEY (`foc_reason`)
+REFERENCES `get_my_parking_v2`.`foc_reasons` (`reason_title`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+-- -----------------------------------------------------
+-- Table `get_my_parking_v2`.`foc_reasons`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `get_my_parking_v2`.`foc_reasons` ;
+
+CREATE TABLE IF NOT EXISTS `get_my_parking_v2`.`foc_reasons` (
+  `reason_title` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`reason_title`))
+  ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `get_my_parking_v2`.`foc_reasons_has_parking_lot`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `get_my_parking_v2`.`foc_reasons_has_parking_lot` ;
+
+CREATE TABLE IF NOT EXISTS `get_my_parking_v2`.`foc_reasons_has_parking_lot` (
+  `foc_reasons_reason_title` VARCHAR(255) NOT NULL,
+  `parking_lot_id` INT NOT NULL,
+  PRIMARY KEY (`foc_reasons_reason_title`, `parking_lot_id`),
+  INDEX `fk_foc_reasons_has_parking_lot_parking_lot1_idx` (`parking_lot_id` ASC),
+  INDEX `fk_foc_reasons_has_parking_lot_foc_reasons1_idx` (`foc_reasons_reason_title` ASC),
+  CONSTRAINT `fk_foc_reasons_has_parking_lot_foc_reasons1`
+  FOREIGN KEY (`foc_reasons_reason_title`)
+  REFERENCES `get_my_parking_v2`.`foc_reasons` (`reason_title`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_foc_reasons_has_parking_lot_parking_lot1`
+  FOREIGN KEY (`parking_lot_id`)
+  REFERENCES `get_my_parking_v2`.`parking_lot` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
+
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
