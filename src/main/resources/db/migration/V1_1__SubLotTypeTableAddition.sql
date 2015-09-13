1c99c84aaeaf18e0b6c51e94f96522a0c19f3f88
Rahul Gupta <rahulgupta.s@flipkart.com>
2015-09-13 18:42:28 +0530

    sub lot type changes

--- src/main/resources/db/migration/V1_1__SubLotTypeTableAddition.sql
+++ src/main/resources/db/migration/V1_1__SubLotTypeTableAddition.sql
@@ -6,10 +6,11 @@ CREATE TABLE IF NOT EXISTS `get_my_parking_v2`.`sub_lot_type` (
 ENGINE = InnoDB;
 
 ALTER TABLE `get_my_parking_v2`.`parking_sub_lot`
-ADD COLUMN `sub_lot_type_name` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `get_my_parking_v2`.`parking_sub_lot`
-  ADD FOREIGN KEY (`sub_lot_type_name`)
+  ADD FOREIGN KEY (`type`)
   REFERENCES `get_my_parking_v2`.`sub_lot_type` (`type_name`);
+
+ALTER TABLE `get_my_parking_v2`.`parking_event`
+ADD FOREIGN KEY (`sub_lot_type`)
+REFERENCES `get_my_parking_v2`.`sub_lot_type` (`type_name`);
 
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
