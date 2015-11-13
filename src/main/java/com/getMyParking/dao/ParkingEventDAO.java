18d65a1b66d4a56e147b368a3de25ce3073437a1
Chirag <chirag333jain333@gmail.com>
2015-11-13 19:50:38 +0530

    search parking events duplicate fix

--- src/main/java/com/getMyParking/dao/ParkingEventDAO.java
+++ src/main/java/com/getMyParking/dao/ParkingEventDAO.java
@@ -396,6 +396,7 @@ public class ParkingEventDAO extends AbstractDAO<ParkingEventEntity> {
         criteria.setFirstResult((pageNum - 1) * pageSize);
         criteria.setMaxResults(pageSize);
         criteria.addOrder(Order.desc("eventTime"));
+        criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 
         return list(criteria);
 
