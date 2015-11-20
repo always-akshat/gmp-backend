f127e57eb54cbdc10e90200ecb2f7382b69b3bbc
Chirag <chirag333jain333@gmail.com>
2015-11-21 02:28:41 +0530

    auto increment changes

--- src/main/resources/db/migration/V2_1_4__PassSchemaChanges.sql
+++ src/main/resources/db/migration/V2_1_4__PassSchemaChanges.sql
@@ -2,5 +2,14 @@
 ALTER TABLE `get_my_parking_v2`.`parking_pass`
 ADD COLUMN `valid_from` TIMESTAMP NULL;
 
+ALTER TABLE parking_sub_lot DROP FOREIGN KEY `fk_parking_sub_lot_parking_lot1`;
+ALTER TABLE parking_event DROP FOREIGN KEY `fk_parking_event_parking_lot1`;
+
 ALTER TABLE `parking_lot` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE parking_sub_lot ADD CONSTRAINT `fk_parking_sub_lot_parking_lot1`
+FOREIGN KEY (`parking_lot_id`) REFERENCES `parking_lot` (`id`);
+ALTER TABLE parking_event ADD CONSTRAINT `fk_parking_event_parking_lot1`
+FOREIGN KEY (`parking_lot_id`) REFERENCES `parking_lot` (`id`);
+
 ALTER TABLE `receipt_content` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
