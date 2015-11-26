19d36c595cde2d5e494155eada6b7efb17043bff
Chirag <chirag333jain333@gmail.com>
2015-11-26 17:39:17 +0530

    adding transaction boundaries in session save task

--- src/main/java/com/getMyParking/task/SessionSaveTask.java
+++ src/main/java/com/getMyParking/task/SessionSaveTask.java
@@ -7,6 +7,7 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.context.internal.ManagedSessionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,18 +35,22 @@ public class SessionSaveTask implements Runnable {
 
     @Override
     public void run() {
+        Session session = sessionFactory.openSession();
+        ManagedSessionContext.bind(session);
+        Transaction transaction = session.beginTransaction();
         try {
-            Session session = sessionFactory.openSession();
-            ManagedSessionContext.bind(session);
             ConcurrentMap<String,GMPUser> contents = sessionCache.asMap();
             contents.forEach((s, gmpUser) -> {
                 sessionDAO.updateSession(gmpUser.getSession());
             });
+            transaction.commit();
             session.flush();
             session.clear();
-            session.close();
         } catch (Exception ex) {
             logger.error("Session Save Task Exception ", ex);
+            if (transaction != null) transaction.rollback();
+        } finally {
+            session.close();
         }
 
     }
