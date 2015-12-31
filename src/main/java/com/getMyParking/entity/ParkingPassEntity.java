b94b17fb2253eb6e8ebd0b7140fe4fbc432928b2
Rahul <rahulgupta.s@flipkart.com>
2015-12-31 17:20:27 +0530

    Revert "added status in parkingpassentity"
    This reverts commit d9336686dcd3afc4dc844085544ec67e79f43378.

--- src/main/java/com/getMyParking/entity/ParkingPassEntity.java
+++ src/main/java/com/getMyParking/entity/ParkingPassEntity.java
@@ -21,7 +21,6 @@ public class ParkingPassEntity {
     private DateTime validFrom;
     private String operatorName;
     private String mobileNumber;
-    private String status;
     @NotNull
     private Integer cost;
     @NotNull
@@ -69,19 +68,6 @@ public class ParkingPassEntity {
     public void setOperatorName(String operatorName) {
         this.operatorName = operatorName;
     }
-
-
-    @Basic
-    @Column(name = "status", nullable = false, insertable = true, updatable = true)
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-
 
     @Basic
     @Column(name = "mobile_number", nullable = false, insertable = true, updatable = true)
